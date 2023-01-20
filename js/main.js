c7458463c97797d7373b7699dfeadf5d717e7110
Sergei Bogko <69677370+sergeibogko@users.noreply.github.com>
2023-01-20 18:13:15 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+});
 const swiper = new Swiper('.swiper', {
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
   },
 });
 
